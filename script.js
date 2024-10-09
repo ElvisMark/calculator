c897a0d7f9c34415653ab3ee857f0f6f925824db
ElvisMark <52351246+ElvisMark@users.noreply.github.com>
2024-10-09 10:32:56 +0300

    created function and tested to see if they work

---
+++ script.js
@@ -0,0 +1,26 @@
+function addition (a,b){
+    return a+b;
+};
+function subtraction(a,b){
+    return a-b;
+};
+function multiplication(a,b){
+    return a*b;
+}
+function division(a,b){
+    return a/b;
+};
+function modulo(a,b){
+    return a%b;
+};
+let result = addition(3,5)
+let result1 = subtraction(13,5)
+let result2 = multiplication(3,5)
+let result3 = division(4,2)
+let result4 = modulo(8,5)
+
+console.log(result)
+console.log(result1)
+console.log(result2)
+console.log(result3)
+console.log(result4)
